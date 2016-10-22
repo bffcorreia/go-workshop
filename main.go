0a5017cbc22c8fdab0c034936f3bcd1eebe72655
Bruno Correia <bffcorreia@gmail.com>
2016-10-22 16:30:31 +0100

    Add routes to get users and create users.

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import (
   "net/http"
   "html/template"
   "github.com/gorilla/mux"
+  "encoding/json"
 )
 
+type User struct {
+  Username string `json:"user"`
+}
+
+var users = []User{User{"admin"}}
 var templates = template.Must(template.ParseGlob("templates/*.tmpl"))
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -29,9 +35,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
   templates.ExecuteTemplate(w, "index", &i)
 }
 
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application.json")
+
+  j, _ := json.Marshal(users)
+  w.Write(j)
+}
+
+func newUsersHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application.json")
+
+  username := mux.Vars(r)["username"]
+  users = append(users, User{ Username: username })
+
+  j, _ := json.Marshal(users)
+  w.Write(j)
+}
+
 func main() { http.HandleFunc("/", handler)
   r := mux.NewRouter()
-  r.HandleFunc("/", handler).Methods("GET")
-  http.ListenAndServe(":8080", nil)
+  r.HandleFunc("/", handler)
+  r.HandleFunc("/users", getUsersHandler).Methods("GET")
+  r.HandleFunc("/users/new/{username:[a-zA-Z]+}", newUsersHandler).Methods("GET")
+  http.ListenAndServe(":8080", r)
 }
 
